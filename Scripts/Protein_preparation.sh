c4e145381a62ca3c904afa8519d82fda340ab243
Chirag Patel <105613466+chiragpatel269@users.noreply.github.com>
2025-01-31 15:24:41 +0400

    Add files via upload

---
+++ Scripts/Protein_preparation.sh
@@ -0,0 +1,4 @@
+ls /opt/schrodinger/
+export SCHRODINGER=/opt/schrodinger/suites2024-4
+ls $SCHRODINGER/utilities/prepwizard
+$SCHRODINGER/utilities/prepwizard -watdist 5.0 -noepik -noprotassign 2a0z.pdb 2a0z.maegz
---
+++ Scripts/xglide_run.sh
@@ -0,0 +1,2 @@
+export SCHRODINGER=/opt/schrodinger/suites2024-4
+"${SCHRODINGER}/run" xglide.py xglide_run.inp -OVERWRITE -HOST localhost:4 -NJOBS 4 -TMPLAUNCHDIR
